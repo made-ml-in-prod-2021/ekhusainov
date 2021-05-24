01850c548b7278442756d270ae1870c01bfb1c53
ekhusainov <xluttiy@gmail.com>
2021-05-24 15:01:37 +0300

    delete unused files

--- online_inference/tests/test_fastapi.py
+++ online_inference/tests/test_fastapi.py
@@ -4,7 +4,7 @@ import pytest
 import numpy as np
 import pandas as pd
 
-from src.fastapi_app import app, check_request, load_trained_model
+from src.fastapi_app import app, check_request
 
 
 from fastapi.testclient import TestClient
