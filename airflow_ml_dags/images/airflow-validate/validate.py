68affc1b8c9ee781bfba11d2429203db2abc96ed
ekhusainov <xluttiy@gmail.com>
2021-06-14 17:24:10 +0300

    add f1_score

--- airflow_ml_dags/images/airflow-validate/validate.py
+++ airflow_ml_dags/images/airflow-validate/validate.py
@@ -1,7 +1,7 @@
 import os
 
 from joblib import load
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 import click
 import pandas as pd
 
@@ -25,11 +25,15 @@ def validate(model_path, validate_path, metric_path,
     target_column = "target"
     y_pred = model.predict(validate_data.drop([target_column], axis=1))
     accuracy_value = accuracy_score(y_pred, validate_data[target_column]) * 100
+    f1_value = f1_score(y_pred, validate_data[target_column]) * 100
     accuracy_text = str(round(accuracy_value, 2))
-    accuracy_text = "accuracy_score: " + accuracy_text + "%"
+    f1_text = str(round(f1_value, 2))
+    accuracy_text = "accuracy_score: " + accuracy_text + "%\n"
+    f1_text = "f1_score: " + f1_text + "%"
     os.makedirs(metric_path, exist_ok=True)
     with open(os.path.join(metric_path, filepath_metric), "w") as file_output:
         file_output.write(accuracy_text)
+        file_output.write(f1_text)
 
 
 if __name__ == "__main__":
